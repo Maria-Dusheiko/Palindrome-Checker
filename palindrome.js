5392ea2995568f045c2d41a093e3677d796e5a8c
Maria Dusheiko <dusheikomaria@gmail.com>
2023-01-21 20:44:07 +0200

    modified:   palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -7,7 +7,7 @@
     let aux = text.toLowerCase().split(/[^a-z0-9]/gi).join('').split('');
      
    if (aux == ""){
-      document.getElementById("answer").innerHTML = "You have not entered text!!";
+      document.getElementById("answer").innerHTML = "You didn't enter text!";
    } else if (aux.join('') == aux.reverse().join('')) {
       document.getElementById("answer").innerHTML = "It is palindrome!";
    } else  {
